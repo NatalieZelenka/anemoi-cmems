c0a8b5bd926da7ee02c266b8daef1302cc53dbad
Natalie Zelenka <natalie.zelenka@ecmwf.int>
2025-09-17 15:06:30 +0100

    Examples of bug

--- anemoi_cmems/cmems.py
+++ anemoi_cmems/cmems.py
@@ -1,7 +1,6 @@
 import earthkit.data as ekd
 import copernicusmarine
 
-from typing import Any
 import xarray
 from datetime import datetime
 
@@ -28,51 +27,54 @@ class CmemsPlugin(Source):
         self.flavour = None
         self.patch = None
 
-    def _download_data(self, dataset_id, param, start: datetime, end: datetime, min_depth, max_depth) -> xarray.Dataset:
+    def _download_data(self, dataset_id, param, start: datetime, end: datetime) -> xarray.Dataset:
         ds = copernicusmarine.open_dataset(
             dataset_id=dataset_id,
             variables=param,
             start_datetime=start,
             end_datetime=end,
-            minimum_depth=min_depth,
-            maximum_depth=max_depth,
             )
+
         return ds
 
-    def _select_depth_levels(self, ds: ekd.FieldList, depth_levels: list[int]) -> ekd.FieldList:
-        ds = ds.sel(depth=depth_levels, method='nearest')
+    def _select_depth_levels(self, ds: ekd.FieldList, depth_index: list[int]) -> ekd.FieldList:
+    
+        depth_levels = [ds.depth.values[i-1] for i in depth_index]
+        ds = ds.sel(depth=depth_levels)
         return ds
 
     def execute(self, dates: DateList, **kwargs) -> ekd.FieldList:
-        param = self.config.get("param", [])      
+        param = self.config.get("param", [])
 
         dataset_id = self.config.get("dataset_id", None)
         if not dataset_id:
             raise ValueError("dataset_id is required")
-
-        depth_levels = self.config.get("depth_levels", [])
-        if not all([isinstance(d, int) for d in depth_levels]):
-            raise ValueError("depth_levels must be a list of integers")
-        
-        depth_levels.sort()
-        if len(depth_levels) != 0:
-            min_depth = max_depth = None
+    
+        depth_indices = self.config.get("depth_indices", [])
+        if not all([isinstance(d, int) for d in depth_indices]):
+            raise ValueError("depth_indices must be a list of integers")
+    
+        if len(dates) != 0:
+            start = dates[0]
+            end = dates[-1]
         else:
-            min_depth = depth_levels[0]
-            max_depth = depth_levels[-1]
-
-        # TODO: is datelist sorted?
+            start = end = None  # For repeated_dates
+            
         ds = self._download_data(
             dataset_id, 
             param, 
-            dates[0],
-            dates[-1], 
-            min_depth, 
-            max_depth,
+            start,
+            end, 
         )
 
-        if len(depth_levels) > 1:
-            ds = self._select_depth_levels(ds, depth_levels)
+        if len(depth_indices) > 1:
+            ds = self._select_depth_levels(ds, depth_indices)
+            assert min(depth_indices) > 0, "depth_indices must be 1 or greater"
+            assert max(depth_indices) <= 50, f"CMEMS has 50 depth levels, not {max(depth_indices)}"
+            assert [int(d) for d in depth_indices] == depth_indices, "depth_indices must be integers"
+        # TODO: Add rename with input print('\n'.join([f"{ds.depth.values[i]}: {i+1}" for i in range(len(ds.depth))]))
+        
+
 
         return load_one("🌊", self.context, dates, ds)
 
--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -3,13 +3,15 @@ from anemoi.datasets.create.testing import TestingContext
 from anemoi.datasets.create import creator_factory
 
 import os
+import pytest
 
 ROOTDIR = os.path.dirname(os.path.dirname(__file__))
 
 def test_plugin():
-    source = create_source(None, "cmems")
+    source = create_source(TestingContext, "cmems")
     assert source is not None
 
+@pytest.mark.skip(reason="Long test, only run manually")
 def test_with_yaml():
     config = os.path.join(ROOTDIR, 'example.yaml')
     output = os.path.join(ROOTDIR, 'example.zarr')
@@ -22,4 +24,4 @@ def test_with_yaml():
     creator_factory("verify", path=output).run()
 
 if __name__ == "__main__":
-    test_plugin()
+   test_with_yaml()
